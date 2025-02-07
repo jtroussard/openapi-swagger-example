14f677e549d0b5b65bbcea33de944345b656b3c4
Jacques Troussard <mm_sparrow@beeswaxnoneofyours.com>
2025-02-07 17:01:05 -0500

    init commit

---
+++ src/main/java/com/devlife4me/openapi_swagger_example/OpenapiSwaggerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.devlife4me.openapi_swagger_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiSwaggerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiSwaggerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlife4me/openapi_swagger_example/OpenapiSwaggerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlife4me.openapi_swagger_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSwaggerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
